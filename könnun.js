a716644a0d198b6b65aae5b8cbf832c5470b8afd
Wowa-penguin <145807570+Wowa-penguin@users.noreply.github.com>
2023-09-25 12:46:26 +0000

    Update könnun.js

--- könnun.js
+++ könnun.js
@@ -1,56 +1,126 @@
 function sínaMyndKyn () {
-    let kyn = '';
-    const kynMynd = `<img src="myndir-fyrir-afrbort/súla kyn.png" alt="">`;
-    kyn = kynMynd;
-    document.getElementById('hlutfarl-mynd-kyn').innerHTML = kyn;
+  let kyn = '';
+  const kynMynd = `<img class="mynd-súla" src="myndir-fyrir-afrbort/súla kyn.png" alt="">`;
+  kyn = kynMynd;
+  let kynButton = '';
+  const kynButtonSín = `<button onclick="afSínaMyndKyn()" class="x-button">X</button>`;
+  kynButton = kynButtonSín;
+  document.getElementById('hlutfarl-mynd-kyn').innerHTML = kyn;
+  document.getElementById('hlutfarl-mynd-kyn-button-x').innerHTML = kynButton;
+};
+
+function afSínaMyndKyn () {
+  document.getElementById('hlutfarl-mynd-kyn').innerHTML = '';
+  document.getElementById('hlutfarl-mynd-kyn-button-x').innerHTML = '';
 };
 
 function sínaMyndAldur () {
-    let aldur = '';
-    const aldurMynd = `<img src="myndir-fyrir-afrbort/súla aldur.png" alt="">`;
-    aldur = aldurMynd;
-    document.getElementById('hlutfarl-mynd-aldur').innerHTML = aldur;
+  let aldur = '';
+  const aldurMynd = `<img class="mynd-súla" src="myndir-fyrir-afrbort/súla aldur.png" alt="">`;
+  aldur = aldurMynd;
+  let aldurButton = '';
+  const aldurButtonSín = `<button onclick="afSínaMyndAldur()" class="x-button">X</button>`;
+  aldurButton = aldurButtonSín;
+  document.getElementById('hlutfarl-mynd-aldur').innerHTML = aldur;
+  document.getElementById('hlutfarl-mynd-aldur-button-x').innerHTML = aldurButton;
+};
+
+function afSínaMyndAldur () {
+  document.getElementById('hlutfarl-mynd-aldur').innerHTML = '';
+  document.getElementById('hlutfarl-mynd-aldur-button-x').innerHTML = '';
 };
 
 function sínaMyndVandamál () {
-    let vandamál = '';
-    const vandamálMynd = `<img src="myndir-fyrir-afrbort/súla stór vandamál.png" alt="">`;
-    vandamál = vandamálMynd;
-    document.getElementById('hlutfarl-mynd-vandamál'). innerHTML = vandamál;
+  let vandamál = '';
+  const vandamálMynd = `<img class="mynd-súla" src="myndir-fyrir-afrbort/súla stór vandamál.png" alt="">`;
+  vandamál = vandamálMynd;
+  let vandamálButton = '';
+  const vandamálButtonSína = `<button onclick="afSínaMyndVandamál()" class="x-button">X</button>`;
+  vandamálButton = vandamálButtonSína;
+  document.getElementById('hlutfarl-mynd-vandamál'). innerHTML = vandamál;
+  document.getElementById('hlutfarl-mynd-vandamál-button-x').innerHTML = vandamálButton;
+};
+
+function afSínaMyndVandamál () {
+  document.getElementById('hlutfarl-mynd-vandamál').innerHTML = '';
+  document.getElementById('hlutfarl-mynd-vandamál-button-x').innerHTML = '';
 };
 
 function sínaMyndKannabis () {
-    let kannabis = '';
-    const kannabisMynd = `<img src="myndir-fyrir-afrbort/súla kannabisefna.png" alt="">`;
-    kannabis = kannabisMynd;
-    document.getElementById('hlutfarl-mynd-kannabis'). innerHTML = kannabis;
+  let kannabis = '';
+  const kannabisMynd = `<img class="mynd-súla" src="myndir-fyrir-afrbort/súla kannabisefna.png" alt="">`;
+  kannabis = kannabisMynd;
+  let kannabisButton = '';
+  const kannabisButtonSína = `<button onclick="afSínaMyndKannabis()" class="x-button">X</button>`;
+  kannabisButton = kannabisButtonSína;
+  document.getElementById('hlutfarl-mynd-kannabis'). innerHTML = kannabis;
+  document.getElementById('hlutfarl-mynd-kannabis-button-x').innerHTML = kannabisButton;
+};
+
+function afSínaMyndKannabis () {
+  document.getElementById('hlutfarl-mynd-kannabis').innerHTML = '';
+  document.getElementById('hlutfarl-mynd-kannabis-button-x').innerHTML = '';
 };
 
 function sínaMyndDæmi () {
-    let dæmi = '';
-    const dæmiMynd = `<img src="myndir-fyrir-afrbort/súla dæma.png" alt="">`;
-    dæmi = dæmiMynd;
-    document.getElementById('hlutfarl-mynd-dæmi'). innerHTML = dæmi;
+  let dæmi = '';
+  const dæmiMynd = `<img class="mynd-súla" src="myndir-fyrir-afrbort/súla dæma.png" alt="">`;
+  dæmi = dæmiMynd;
+  let dæmiButtan = '';
+  const dæmiButtanSína = `<button onclick="afSínaMyndDæmi()" class="x-button">X</button>`;
+  dæmiButtan = dæmiButtanSína;
+  document.getElementById('hlutfarl-mynd-dæmi'). innerHTML = dæmi;
+  document.getElementById('hlutfarl-mynd-dæmi-button-x'). innerHTML = dæmiButtan;
+};
+function afSínaMyndDæmi () {
+  document.getElementById('hlutfarl-mynd-dæmi').innerHTML = '';
+  document.getElementById('hlutfarl-mynd-dæmi-button-x').innerHTML = '';
 };
 
 function sínaMyndRefsa () {
-    let refsa = '';
-    const refsaMynd = `<img src="myndir-fyrir-afrbort/súla refsa.png" alt="">`;
-    refsa = refsaMynd;
-    document.getElementById('hlutfarl-mynd-refsa'). innerHTML = refsa;
+  let refsa = '';
+  const refsaMynd = `<img class="mynd-súla" src="myndir-fyrir-afrbort/súla refsa.png" alt="">`;
+  refsa = refsaMynd;
+  let = refsaButton = '';
+  const refsaButtonSína = `<button onclick="afSínaMyndRefsa()" class="x-button">X</button>`;
+  refsaButton = refsaButtonSína;
+  document.getElementById('hlutfarl-mynd-refsa'). innerHTML = refsa;
+  document.getElementById('hlutfarl-mynd-refsa-button-x'). innerHTML = refsaButton;
+};
+
+function afSínaMyndRefsa () {
+  document.getElementById('hlutfarl-mynd-refsa').innerHTML = '';
+  document.getElementById('hlutfarl-mynd-refsa-button-x').innerHTML = '';
 };
 
 function sínaMyndMálið () {
-    let málið = '';
-    const máliðMynd = `<img src="myndir-fyrir-afrbort/súla stórmálið.png" alt="">`;
-    málið = máliðMynd;
-    document.getElementById('hlutfarl-mynd-málið'). innerHTML = málið;
+  let málið = '';
+  const máliðMynd = `<img class="mynd-súla" src="myndir-fyrir-afrbort/súla stórmálið.png" alt="">`;
+  málið = máliðMynd;
+  let máliðButton = '';
+  const máliðButtonSína = `<button onclick="afSínaMyndmálið()" class="x-button">X</button>`;
+  máliðButton = máliðButtonSína;
+  document.getElementById('hlutfarl-mynd-málið'). innerHTML = málið;
+  document.getElementById('hlutfarl-mynd-málið-button-x'). innerHTML = máliðButton
+};
+
+function afSínaMyndmálið () {
+  document.getElementById('hlutfarl-mynd-málið').innerHTML = '';
+  document.getElementById('hlutfarl-mynd-málið-button-x').innerHTML = '';
 };
 
 function sínaMyndHluti () {
-    let hlut = '';
-    const hlutMynd = `<img src="myndir-fyrir-afrbort/súla stór hluti.png" alt="">`;
-    hlut = hlutMynd;
-    document.getElementById('hlutfarl-mynd-hluti'). innerHTML = hlut;
+  let hlut = '';
+  const hlutMynd = `<img class="mynd-súla" src="myndir-fyrir-afrbort/súla stór hluti.png" alt="">`;
+  hlut = hlutMynd;
+  let hlutButton = '';
+  const hlutButtonSína = `<button onclick="afSínaMyndHlut()" class="x-button">X</button>`;
+  hlutButton = hlutButtonSína;
+  document.getElementById('hlutfarl-mynd-hluti'). innerHTML = hlut;
+  document.getElementById('hlutfarl-mynd-hluti-button-x'). innerHTML = hlutButton;
 };
 
+function afSínaMyndHlut () {
+  document.getElementById('hlutfarl-mynd-hluti').innerHTML = '';
+  document.getElementById('hlutfarl-mynd-hluti-button-x').innerHTML = '';
+};
